35ada5237c07ba42298ea3de7718ed6591ec2b89
LGear1 <leoghe03@gmail.com>
2022-07-19 11:28:28 +0200

    Progetto iniziale

---
+++ src/test/java/it/rjcsoft/gherardi/ProgettoSpringLeo/ProgettoSpringLeoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.rjcsoft.gherardi.ProgettoSpringLeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSpringLeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
