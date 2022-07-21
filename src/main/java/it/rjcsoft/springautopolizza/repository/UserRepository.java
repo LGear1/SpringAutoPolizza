a9da8de1919cf193914a959489cefd246713b0a1
LucaBracaloni <l.bracaloni@rjcsoft.it>
2022-07-21 17:18:52 +0200

    create Delete and SelectUser

--- src/main/java/it/rjcsoft/springautopolizza/repository/UserRepository.java
+++ src/main/java/it/rjcsoft/springautopolizza/repository/UserRepository.java
@@ -8,7 +8,7 @@ import it.rjcsoft.springautopolizza.model.User;
 public interface UserRepository {
     int insertUser(String name, String surname, String email, String password, String cf, Date dateOfBirth, int role);
     int deleteUser(String cf);
-    List<User> selectUser(int id);
+    List<User> selectUser(String cf);
     List<User> selectAllUsers();
     boolean updateUser(String name, String surname, String cf, Date dateOfBirth, int role);
 
--- src/main/java/it/rjcsoft/springautopolizza/repository/UserRepositoryImpl.java
+++ src/main/java/it/rjcsoft/springautopolizza/repository/UserRepositoryImpl.java
@@ -1,19 +1,27 @@
 package it.rjcsoft.springautopolizza.repository;
 
+import it.rjcsoft.springautopolizza.model.Auto;
 import it.rjcsoft.springautopolizza.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
 import java.sql.Date;
 import java.util.List;
 
+@Repository
 public class UserRepositoryImpl implements UserRepository{
 
     private String QueryInsertCredenziali="Insert into test1_credenziali (email,pwd,iduser) VALUES (?,?,?)";
     private String QueryInsertUser="Insert into test1_users (nome, cognome, cf, datanascita, ruolo_id) VALUES (?,?,?,?,?)";
 
-    @Autowired
+    private String QueryDeleteUser="DELETE FROM test1_users WHERE id = ?";
+
+    private String QuerySelectUser="Select * from test1_users tu JOIN test1_roles tr ON tr.id=ruolo_id JOIN test1_credenziali tc ON tc.iduser=tu.id WHERE tu.id = ?";
+
+    private String QuerySelectUser2="Select * from test1_users tu WHERE tu.cf = ?";
     private JdbcTemplate jdbcTemplate;
     @Override
     public int insertUser(String name, String surname, String email, String password, String cf, Date dateOfBirth, int role) {
@@ -30,13 +38,25 @@ public class UserRepositoryImpl implements UserRepository{
     }
 
     @Override
-    public boolean deleteUser(String cf) {
-        return false;
+    public int deleteUser(String cf) {
+        Object[] args = new Object[] {cf};
+        return jdbcTemplate.update(QueryDeleteUser, args);
     }
 
+    RowMapper<User> rowMapper = (rs, rowNum) -> {
+        User user = new User();
+        user.setName(rs.getString("Nome"));
+        user.setSurname(rs.getString("Surname"));
+        user.setEmail(rs.getString("Email"));
+        user.setPassword(rs.getString("Password"));
+        user.setCf(rs.getString("cf"));
+        user.setDateOfBirth(rs.getDate("dateOfBirth"));
+        return user;
+    };
     @Override
-    public List<User> selectUser(int id) {
-        return null;
+    public List<User> selectUser(String cf){
+        Object[] args = new Object[] {cf};
+        return jdbcTemplate.query(QuerySelectUser2, rowMapper ,args);
     }
 
     @Override
