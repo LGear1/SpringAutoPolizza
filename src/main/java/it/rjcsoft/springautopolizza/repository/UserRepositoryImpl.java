6338baac26177f4c85f53acc897e9e1d7326c3db
Fatta Salvatore <fatta.salvatore02@gmail.com>
2022-07-21 16:58:44 +0200

    Aggiunto insertUser

--- src/main/java/it/rjcsoft/springautopolizza/repository/UserRepositoryImpl.java
+++ src/main/java/it/rjcsoft/springautopolizza/repository/UserRepositoryImpl.java
@@ -1,15 +1,32 @@
 package it.rjcsoft.springautopolizza.repository;
 
 import it.rjcsoft.springautopolizza.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.sql.Date;
 import java.util.List;
 
 public class UserRepositoryImpl implements UserRepository{
 
+    private String QueryInsertCredenziali="Insert into test1_credenziali (email,pwd,iduser) VALUES (?,?,?)";
+    private String QueryInsertUser="Insert into test1_users (nome, cognome, cf, datanascita, ruolo_id) VALUES (?,?,?,?,?)";
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
     @Override
     public int insertUser(String name, String surname, String email, String password, String cf, Date dateOfBirth, int role) {
-        return 0;
+        try {
+            jdbcTemplate.update(QueryInsertCredenziali,
+                    new Object[] {email, password });
+            jdbcTemplate.update(QueryInsertUser,
+                    new Object[] {name, surname, cf, dateOfBirth, role  });
+            return 1;
+        } catch (DataIntegrityViolationException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
