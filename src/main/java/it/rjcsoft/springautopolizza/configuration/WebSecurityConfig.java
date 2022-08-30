b201941dac978c926a0eb3d3b9af13ec182edec2
Fatta Salvatore <fatta.salvatore02@gmail.com>
2022-08-30 17:03:30 +0200

    Aggiunto SpringBoot Security con Auth

---
+++ src/main/java/it/rjcsoft/springautopolizza/configuration/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package it.rjcsoft.springautopolizza.configuration;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+import java.util.Base64;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder authBuilder) throws Exception{
+        authBuilder.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .usersByUsernameQuery("" +
+                        "SELECT email, pwd, enable " +
+                        "FROM test1_credenziali " +
+                        "WHERE email=?")
+                .authoritiesByUsernameQuery("Select * from test1_users tu JOIN test1_roles tr ON tr.id=ruolo_id JOIN test1_credenziali tc ON tc.iduser=tu.id WHERE tc.email = ?");
+    }
+
+    @Override
+    protected  void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers("/*").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .logout().permitAll()
+                .and()
+                .exceptionHandling().accessDeniedPage("/403")
+        ;
+    }
+
+
+
+}
