1389559c0bd25ff3d6d082ce3e123d9d464011fa
Fatta Salvatore <fatta.salvatore02@gmail.com>
2022-08-31 14:40:04 +0200

    Fix del redirect pagina 403/404

--- src/main/java/it/rjcsoft/springautopolizza/configuration/WebSecurityConfig.java
+++ src/main/java/it/rjcsoft/springautopolizza/configuration/WebSecurityConfig.java
@@ -31,19 +31,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
+    protected  void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests()
-                .antMatchers("/auto/**").hasRole("ADMIN")
+                .antMatchers("/auto/**").hasAuthority("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().permitAll()
                 .and()
                 .logout().permitAll()
                 .and()
-                .exceptionHandling().accessDeniedPage("/403");
+                .exceptionHandling().accessDeniedPage("/403")
         ;
     }
-
-
-
 }
