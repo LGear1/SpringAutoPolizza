729a801a2eefa93cb53cd8cb25ab3b0e552af064
Fatta Salvatore <fatta.salvatore02@gmail.com>
2022-08-31 09:52:09 +0200

    Aggiunto Redirect

--- src/main/java/it/rjcsoft/springautopolizza/configuration/WebSecurityConfig.java
+++ src/main/java/it/rjcsoft/springautopolizza/configuration/WebSecurityConfig.java
@@ -33,7 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected  void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests()
-                .antMatchers("/*").hasRole("ADMIN")
+                .antMatchers("/auto").hasRole("Admin")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().permitAll()
--- src/main/java/it/rjcsoft/springautopolizza/controller/LoginController.java
+++ src/main/java/it/rjcsoft/springautopolizza/controller/LoginController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -57,4 +58,9 @@ public class LoginController {
         }
     }
 
+    @GetMapping
+    public String callL() {
+        return "Ciao";
+
+    }
 }
